ece35939c1d790213ea49ebd965c9ba3555e8567
Gamunited1 <nfansujanneh@gmail.com>
2025-11-29 18:39:21 -0700

    This is my second gethub command

---
+++ HelpDesk.py
@@ -0,0 +1,8 @@
+#Simple Pyton Program to Greet Nfansu Jenneh
+
+def greet_user():
+    name = input("Is this the Help Desk Project? ")
+    print(f"Yes, {name}! This is your Help Desk Project you created for the assingment.")
+
+# Call the function
+greet_user()
